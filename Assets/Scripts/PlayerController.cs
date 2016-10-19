79a245d2d3be0f68314d550a23ecfdc9f21b9683
sarasolano <sarasolano10@gmail.com>
2016-10-19 13:25:56 +0200

    easter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	public float speed;
 	private Vector3 dir;
+	public int pickUpCount;
+	public Text countText;
+	private Rigidbody rb;
+	private int count;
 
 	void Start() {
 		dir = Vector3.zero;
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
 	}
 
 	void Update() {
@@ -23,45 +30,23 @@ public class PlayerController : MonoBehaviour {
 		//		transform.Rotate (dir * amountToMove);
 		transform.Translate (dir * amountToMove);
 	}
+		
+	//****we may want to move this to another class!****
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.CompareTag("EasterEgg")){
+			Destroy (other.gameObject);
+			count++;
+			SetCountText ();
+		}
+		if (other.gameObject.CompareTag("Jackolantern")){
+			Destroy (other.gameObject);
+			count++;
+			SetCountText ();
+		}
+		//if hit skeleton -- lose points?!
+	}
 
-//	public float speed;
-//	public int pickUpCount;
-//
-//	public Text countText;
-//	private Rigidbody rb;
-//	private int count;
-//
-//	void Start() {
-//		rb = GetComponent<Rigidbody>();
-//		count = 0;
-//		SetCountText ();
-//	}
-//
-//	void FixedUpdate () {
-//		float moveHorizontal = Input.GetAxis ("Horizontal");
-//		float moveVertical = Input.GetAxis ("Vertical");
-//
-//
-//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-//		rb.AddForce (movement * speed);
-//	}
-//
-//	//****we may want to move this to another class!****
-//	void OnTriggerEnter (Collider other){
-//		if (other.gameObject.CompareTag("EasterEgg")){
-//			Destroy (other.gameObject);
-//			count++;
-//			SetCountText ();
-//		}
-//		if (other.gameObject.CompareTag("Jackolantern")){
-//			Destroy (other.gameObject);
-//			count++;
-//			SetCountText ();
-//		}
-//		//if hit skeleton -- lose points?!
-//	}
-//
-//	void SetCountText (){
-//		countText.text = "Count: " + count.ToString ();
-//	}
+	void SetCountText (){
+		countText.text = "Count: " + count.ToString ();
+	}
 }
