002ee6a48ff050d5d31f34e589250114754b8fac
Ashley Simons <acs32014@mymail.pomona.edu>
2016-10-19 15:53:21 +0200

    on your mark text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,13 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	private float storedSpeed;
 	private Vector3 dir;
+
 	public int pickUpCount;
 	public Text countText;
+	public Text onYourMarkText;
+
 	private Rigidbody rb;
 	private int count;
 
@@ -14,9 +18,26 @@ public class PlayerController : MonoBehaviour {
 		dir = Vector3.zero;
 		rb = GetComponent<Rigidbody>();
 		count = 0;
+		storedSpeed = speed;
+		onYourMarkText.text = "";
 //		SetCountText ();
+		StartCoroutine(OnYourMark());
 	}
 
+	//wait for 5 seconds to start the game & display starting text
+	IEnumerator OnYourMark() {
+		this.speed = 0;
+		onYourMarkText.text = "On Your Mark";
+		yield return new WaitForSeconds (2);
+		onYourMarkText.text = "Get Set";
+		yield return new WaitForSeconds (2);
+		onYourMarkText.text = "Go!!";
+		yield return new WaitForSeconds (1);
+		onYourMarkText.text = "";
+		this.speed = storedSpeed;
+	}
+
+	//controls the movement of the ball
 	void Update() {
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			dir = Vector3.right;
